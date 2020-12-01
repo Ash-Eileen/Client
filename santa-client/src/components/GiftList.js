77bbbc61e044cafe1b720802a1d91e8a4d849c1a
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-12-01 12:23:31 +1100

    gift lists delete and add, added

--- santa-client/src/components/GiftList.js
+++ santa-client/src/components/GiftList.js
@@ -1,8 +1,28 @@
-const GiftList = () =>{  
+import List from "./List"   
+
+
+
+
+
+const GiftList = (props) =>{    
+
+    const addList = () =>{  
+        // add stuff from inuts 
+    }
 
     return(  
         <div> 
-            <h1>GiftList</h1>
+            <h1>GiftList</h1> 
+
+            <a onClick={addList}>add person</a> 
+            <div>  
+                <List  
+                name= "ye" 
+                items = "BETTER"
+                />
+
+            </div>  
+            
         </div>
     )
 }
