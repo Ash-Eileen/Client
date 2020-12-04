5f4b078d095ea5799d59845e7d5afb1cd3a53c56
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-12-04 11:53:07 +1100

    list and gift list component fully functional :)

--- santa-client/src/components/GiftList.js
+++ santa-client/src/components/GiftList.js
@@ -1,5 +1,6 @@
 import List from "./List"    
-import {useGlobalState} from '../config/store' 
+import {useGlobalState} from '../config/store'  
+import { v4 as uuidv4 } from 'uuid';
 
 
 
@@ -9,8 +10,9 @@ import {useGlobalState} from '../config/store'
 const GiftList = (props) =>{     
 
     const {store,dispatch} = useGlobalState() 
-    const {giftLists} = store  
-    
+    const {giftLists} = store   
+
+    let randomId = uuidv4() 
 
     const addList = (event) =>{  
         
@@ -19,17 +21,8 @@ const GiftList = (props) =>{
         
         
         event.preventDefault()   
-        
-
-        // let person = {} 
-        // person[ranNum] = []   
-        
-        // let giftListsCopy = {...giftLists, person}  
-
-        giftLists[Object.keys(giftLists).length] = []
-        // console.log(giftListsCopy)
-
-        // console.log(Object.keys(giftLists))
+         
+        giftLists[randomId] = [] 
 
         dispatch ({ 
             type:"setGiftLists", 
@@ -52,11 +45,6 @@ const GiftList = (props) =>{
         console.log(giftLists)
     }
 
-// delete Object.keys(giftLists)[1] 
-
-// list : remove a specific index position from the list array
-// giftList : remove all the fields in the giftList state that corresponds to a particular person
-
     return(  
         <div> 
             <h1>GiftList</h1> 
@@ -64,12 +52,9 @@ const GiftList = (props) =>{
             <button onClick={addList}>Add Gift List</button> 
             
             <div>    
-                {/* <List /> 
-                <button onClick={deleteList}>Delete List</button>  */}
                 
                 {Object.keys(giftLists).length > 0 && Object.keys(giftLists).map((v, i) => {  
-                    // return <div><List identifer={v}/>,<button id={v} onClick={deleteList}>Delete List</button> </div> 
-                    return <div><List identifer={v}/>,<button id={v} onClick={deleteList}>Delete List</button> </div>
+                    return <div><List identifer={v}/>,<button id={v} onClick={deleteList}>Delete List</button> </div>         
                     })}
                 
             </div>  
--- santa-client/src/components/List.js
+++ santa-client/src/components/List.js
@@ -1,22 +1,15 @@
 import "../styles/styles.scss"
-import {useGlobalState} from '../config/store'   
-import React, {useState} from 'react' 
-
- 
+import {useGlobalState} from '../config/store'    
 
 const List = (props) =>{   
 
     const {store, dispatch} = useGlobalState() 
     const {giftLists} = store  
-
-    const [formName,setFormName] = useState("name")  
-
     const {identifer} = props  
 
 
 
 //   deletes a selected gift and updates global state
- 
 const deleteGift = (event) =>{ 
     event.preventDefault()  
     
@@ -28,15 +21,9 @@ const deleteGift = (event) =>{
             })  
   }   
 
-//   displays items in giftLists 
-// (Object.keys(giftLists).length !== 0)
+
+//   displays gifts in list from global state
     const showItems = () => {   
-        // console.log(giftLists[formName], formName)
-        // if ( (giftLists[identifer]) && (identifer !== "name" || identifer !== "") ) { 
-        //     return giftLists[document.getElementById(identifer).value].map((v,i)=>{ 
-        //         return <div key={i}> <li>{v}</li> <button index={i} onClick={deleteGift}>delete</button></div>
-        //     })
-        // } 
 
         if ( (giftLists[identifer].length > 0 ) ) { 
             return giftLists[identifer].map((v,i)=>{ 
@@ -45,33 +32,14 @@ const deleteGift = (event) =>{
         }
     } 
 
-//    adds items to list via update of global state and saves list
-    const addItem = (event) => {    
-        event.preventDefault()  
 
-        // let name = event.target.name.value
+// adds a gift to the global state
+    const addItem = (event) => {    
+        event.preventDefault()   
 
         if (giftLists[identifer]) {  
-            
-            // let newId = `${identifer + event.target.name.value}`
-
-            // giftLists[identifer + event.target.name.value] = giftLists[identifer];
-            //  delete giftLists[ identifer ];   
-             
-            //  console.log(newId)
-
-            //  dispatch({ 
-            //     type: "setGiftLists", 
-            //     data: giftLists
-            // })   
-
-             
-
-            // giftLists[identifer + event.target.addItem.value].push(event.target.addItem.value) 
-
 
             giftLists[identifer].push(event.target.addItem.value)
-
 
               dispatch({ 
                 type: "setGiftLists", 
@@ -79,22 +47,7 @@ const deleteGift = (event) =>{
             })    
             
         } 
-            
-        // } else {   
-        //     let giftListCopy = {...giftLists, [name]: [event.target.addItem.value]}  
 
-        //        dispatch({
-        //         type: "setGiftLists", 
-        //         data: giftListCopy
-        //     })    
-        
-        //     document.getElementById(formName).setAttribute("id", `${name}`)   
-        //     setFormName(name) 
-
-        // }    
-
-        // document.getElementById(formName).setAttribute("id", `${name}`)   
-        //  setFormName(name) 
     } 
 
 
@@ -106,7 +59,6 @@ const deleteGift = (event) =>{
         <form onSubmit={addItem}>
         Name:   
         <input type="text" id="name" name="name" required />
-        {/* {formName == "name" ? <input type="text" id="name" name="name" /> : <h4 id={formName}>{formName}</h4>} */}
         <br/>   
          
         {showItems()}
